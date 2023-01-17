1788887582cfcf62a1d437edbd8e6ea61e862751
Isaac Reign <corbinisaac99@email.com>
2023-01-18 07:01:50 +0900

    fix medias

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
