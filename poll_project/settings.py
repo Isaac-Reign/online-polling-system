6d6168907fcee44189f3147eb89daf8a22004bcf
Isaac Reign <corbinisaac99@email.com>
2023-01-18 03:10:08 +0900

    all specify

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
