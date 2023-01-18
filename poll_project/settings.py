913490e1fb487b260b95fa29d3803eb91b8b6a94
Isaac Reign <corbinisaac99@email.com>
2023-01-18 15:38:41 +0900

    remote

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -122,9 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
