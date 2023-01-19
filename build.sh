541a83874fe25cf81598c8a120562a84666a9e49
Isaac Reign <corbinisaac99@email.com>
2023-01-19 17:12:00 +0900

    s

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
-then
-	python manage.py createsuperuser --no-input
-fi	
+	
+	then
+		python manage.py createsuperuser --no-input
+	fi	
