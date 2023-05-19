d76179571d575b8df4e5d8adfa381bc009fb2fe7
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2023-05-19 16:22:33 -0700

    Update settings.py

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG"))
-
+#DEBUG = str(os.getenv("DEBUG"))
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
