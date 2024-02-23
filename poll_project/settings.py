708b5ec368b376dee38c2558273c9be081c4d237
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2024-02-23 22:26:23 +0000

    Update settings.py
    Debugged set

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.getenv("DEBUG"))
+#DEBUG = str(os.getenv("DEBUG"))
 
 # if (len(sys.argv) >= 2 and sys.argv[1] == 'runserver'):
 #    DEBUG = True
 # else:
 #    DEBUG = False
-
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
