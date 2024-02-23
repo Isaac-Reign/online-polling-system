9553eef5f0abeb2c743695c5898cd16c0acc8da0
Isaac Reign <122174090+Isaac-Reign@users.noreply.github.com>
2024-02-23 20:03:03 +0000

    Update settings.py

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,14 +130,17 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+ STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'poll/static')
+ ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Default primary key field type
